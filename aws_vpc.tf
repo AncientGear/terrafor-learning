9f45b5eefa169bf41c1b62d346dd97799db0b7fc
saul <cpp.saul@gmail.com>
2025-07-30 15:18:50 -0600

    removing ipv6 egress SG

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -85,9 +85,3 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
     Name = "allow egress ssh"
   }
 }
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.sg_public_instance.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
